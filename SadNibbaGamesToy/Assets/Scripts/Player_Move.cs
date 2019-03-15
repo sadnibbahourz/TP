15e3a27b727d4f737b0f7b02aed1415d83a56120
Alfenstein <alfredmakne@gmail.com>
2019-03-15 15:04:05 +0100

    Lavet movement script og er begyndt på Jump funtionen, men der mangler et ContactFilter2D

---
+++ SadNibbaGamesToy/Assets/Scripts/Player_Move.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    #region refrences
+    public Rigidbody2D rb;
+    public Transform spawnPoint;
+    #endregion
+
+    #region Variabler
+    float move;
+    public float speed;
+    public float maxSpeed = 5;
+    public float jumpForce = 20;
+    #endregion
+    void Start()
+    {
+        transform.position = spawnPoint.position;
+    }
+
+    
+    void FixedUpdate()
+    {
+        Walk();
+        if (Groundcheck())
+        {
+            Jump();
+        }
+    }
+
+
+    #region Functions
+
+    void Walk()
+    {
+        move = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        rb.AddForce(new Vector2(move, 0));
+
+        if (rb.velocity.x > maxSpeed)
+        {
+            rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+        }
+        if (rb.velocity.x < -maxSpeed)
+        {
+            rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
+        }
+    }
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x,jumpForce);
+        }
+    }
+    bool Groundcheck()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down,transform.lossyScale.y/2); // Der skal addes et ContactFilter2D
+    }
+    #endregion
+}
