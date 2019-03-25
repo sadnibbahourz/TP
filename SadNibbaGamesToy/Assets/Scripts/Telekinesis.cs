a3429c389c714f8884e79808b975a9977ccbe4ec
Alfenstein <alfredmakne@gmail.com>
2019-03-25 17:57:01 +0100

    Jeg slettede nogle Debug.Logs

--- SadNibbaGamesToy/Assets/Scripts/Telekinesis.cs
+++ SadNibbaGamesToy/Assets/Scripts/Telekinesis.cs
@@ -13,8 +13,6 @@ public class Telekinesis : MonoBehaviour
     #endregion
     private void OnMouseDrag()
     {
-        Debug.Log(new Vector2(acceleration, rb.velocity.y) * Vector2.right);
-        Debug.Log(new Vector2(acceleration, rb.velocity.y) * Vector2.left);
         if(Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x,0)).x > transform.position.x)
         {
             rb.AddForce(new Vector2(acceleration, 0)* Vector2.right);
