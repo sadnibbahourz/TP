1fc1bf38fa5759599a2a5876d5919faac0d50042
Alfenstein <alfredmakne@gmail.com>
2019-05-01 20:37:38 +0200

    Restart knap på ESC Gjort lvl_1 mindre Kendte fejl: Box opføre sig underligt når at man går ind i den og det rammer noget Kanter driller når man går ind i dem Boxen kan bevæges af player

---
+++ SadNibbaGamesToy/Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    #region Refrences
+    public KeyCode restart;
+
+    #endregion
+
+    #region Variables
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(restart))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
