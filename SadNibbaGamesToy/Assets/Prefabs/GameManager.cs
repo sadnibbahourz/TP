d264980b07c67b361e08cba990e5a039f1a6e491
Alfenstein <alfredmakne@gmail.com>
2019-05-28 21:59:52 +0200

    Level design

--- SadNibbaGamesToy/Assets/Prefabs/GameManager.cs
+++ SadNibbaGamesToy/Assets/Prefabs/GameManager.cs
@@ -7,13 +7,17 @@ public class GameManager : MonoBehaviour
 {
     #region Refrences
     public KeyCode restart;
-
+    public GameObject player;
     #endregion
 
     #region Variables
-
+    [Tooltip("The lowest the player can go without dieing")]
+    public float lowest;
     #endregion
     // Start is called before the first frame update
+
+
+    
     void Start()
     {
         
@@ -22,7 +26,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(restart))
+        if (Input.GetKeyDown(restart) || player.transform.position.y <= lowest)
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
