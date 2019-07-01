307d889f25f57ed953d0fac7cbe29bcdb6e0bc7e
DiscoLucas <47451042+DiscoLucas@users.noreply.github.com>
2019-07-01 11:49:09 +0200

    Made The parallax script work

--- SadNibbaGamesToy/Assets/Scripts/Parallax.cs
+++ SadNibbaGamesToy/Assets/Scripts/Parallax.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private float length, startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+
     void Start()
     {
-        
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
     }
 }
